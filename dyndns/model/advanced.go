3e1a3b48c906c1de2879d6f1ceabf38de787e3af
xris99 <79798089+xris99@users.noreply.github.com>
2022-06-05 20:16:51 +0200

    Update advanced.go

--- dyndns/model/advanced.go
+++ dyndns/model/advanced.go
@@ -9,6 +9,7 @@ type Advanced struct {
 	gorm.Model
 	Hostname string `gorm:"not null" form:"hostname" validate:"required,hostname"`
 	Value   string   `gorm:"not null" validate:"required,hostname"`
+	Type   string   `gorm:"not null" validate:"required,hostname"`
 	TargetID uint
 	Ttl      int `form:"ttl" validate:"required,min=20,max=86400"`
 }
